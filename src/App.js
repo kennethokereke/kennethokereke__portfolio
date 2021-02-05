c846195d74293d4f8bd8869f7be37b47c044c7c2
Kenneth Okereke <kenneth.okereke@wagner.edu>
2021-02-05 15:21:50 -0500

    new change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import {Router} from 'react-router-dom'
+import history from '../src/history'
+import Navbar from '../src/Components/Navbar';
+import ContentWrapper from './Components/ContentWrapper'
+import './App.scss'
+
 
 function App() {
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+     <Router history={history}>
+     <Navbar/>
+     <ContentWrapper/>
+     </Router>
     </div>
   );
 }
 
 export default App;
+ 
